eb85887b5479eb9ac477bdfc6258d76d5bfcdb89
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-10-10 20:22:56 +0900

    단축 속성명의 디버깅

---
+++ SpreadOperator/3.js
@@ -0,0 +1,9 @@
+const name = 'mike';
+const age = 21;
+console.log(name, age);
+console.log('name=', name, ', age =', age);
+console.log({ name, age });
+//단축 속성명을 이용하면 디버깅도 유리하다.
+//첫번째 콘솔로그는 어떤것인지 알아보기 힘들다.
+//두번째는 작성이 불편하다
+//3번째처럼 단축 속성명을 이용하면 편리하고 알아보기 쉽게 디버깅 할 수 있다.
